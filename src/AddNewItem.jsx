02de1c26ffb51a1853313d4a2bb306a708c94669
EfeHa <efehasircioglu.com@gmail.com>
2025-08-22 22:10:07 +0300

    a better state

--- src/AddNewItem.jsx
+++ src/AddNewItem.jsx
@@ -18,11 +18,33 @@ export default function AddNewItem({ taskList, setTasks }) {
     ]); /* burada varolan array + yeni itemimiz ile yeni bir array oluşturuyoruz */
   }
   return (
-    <button
-      onClick={handleClick}
-      className="border-1 border-dashed rounded-md justify-self-center w-[97%] p-1 m-1 cursor-pointer"
-    >
-      <span>Add new item +</span>
+    <button onClick={handleClick} className="z-50 cursor-pointer">
+      <span>
+        <svg
+          width="21px"
+          height="21px"
+          stroke-width="1.5"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          color="#fff"
+        >
+          <path
+            d="M8 12H12M16 12H12M12 12V8M12 12V16"
+            stroke="#fff"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
+          <path
+            d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
+            stroke="#fff"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>
+        </svg>
+      </span>
     </button>
   );
 }
--- src/List.jsx
+++ src/List.jsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from "motion/react";
 import AddNewItem from "./addNewItem";
 import ListItem from "./ListItem";
 
@@ -16,31 +17,38 @@ export default function List({
   };
   return (
     <div
-      className={`m-4 h-auto rounded-md group shadow-2xl shadow-black ${bgColors[bgColor]}`}
+      className={`m-4 h-min rounded-md group shadow shadow-black z-50 ${bgColors[bgColor]}`}
     >
-      <div className="flex flex-col">
-        <div className="px-4 py-2">{header}</div>
-        {taskList
-          .filter((task) => task.status === status)
-          .map((task) => (
-            <ListItem
-              key={task.id}
-              task={task}
-              setTasks={setTasks}
-              taskList={taskList}
-            />
-          ))}
-        {addSection && (
-          <div
-            className={`${
-              taskList.length === 0
-                ? "block self-center"
-                : "opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-50"
-            }`}
-          >
-            <AddNewItem taskList={taskList} setTasks={setTasks} />
+      <div className="flex flex-col z-50">
+        <div className="px-4 z-50 py-2 flex flex-row justify-between items-center">
+          <div>{header}</div>
+          <div>
+            {addSection && (
+              <div className="z-50">
+                <AddNewItem taskList={taskList} setTasks={setTasks} />
+              </div>
+            )}
           </div>
-        )}
+        </div>
+        <AnimatePresence>
+          {taskList
+            .filter((task) => task.status === status)
+            .map((task) => (
+              <motion.div
+                key={task.id}
+                initial={{ opacity: 0, y: -2 }}
+                animate={{ opacity: 1, y: 2 }}
+                exit={{ opacity: 0, y: 10 }}
+              >
+                <ListItem
+                  key={task.id}
+                  task={task}
+                  setTasks={setTasks}
+                  taskList={taskList}
+                />
+              </motion.div>
+            ))}
+        </AnimatePresence>
       </div>
     </div>
   );
